c3b9971fd954049aa8090a384b64543e179931b5
Ivan Davidyuk <vanyadavidyuk98@mail.ru>
2023-09-06 19:34:44 +0300

    fix

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -12,24 +12,32 @@ class Main extends React.Component {
     };
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=iron-man`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=iron-man`)
             .then((response) => response.json())
             .then((data) =>
                 this.setState({ movies: data.Search, loading: false })
-            );
+            )
+            .catch((err) => {
+                console.error(err);
+                this.setState({ loading: false });
+            });
     }
 
     searchMovies = (str, type = 'all') => {
         this.setState({ loading: true });
         fetch(
-            `http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
+            `https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${
                 type !== 'all' ? `&type=${type}` : ''
             }`
         )
             .then((response) => response.json())
             .then((data) =>
                 this.setState({ movies: data.Search, loading: false })
-            );
+            )
+            .catch((err) => {
+                console.error(err);
+                this.setState({ loading: false });
+            });
     };
 
     render() {
